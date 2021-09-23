e868ca7683ff6848da089463935fec4cf52d61a0
Ryan Cleven <rcleven@mac.com>
2021-09-22 20:11:39 -0700

    Fixed compiling against new library name

--- test/NoGUI/run_all_test.bat
+++ test/NoGUI/run_all_test.bat
@@ -1,17 +1,17 @@
 @echo off
 
 echo "Running TestGravity"
-haxe -lib bullet -lib heaps --main TestGravity --hl compiled.hl
-hl compiled.hl
+haxe -lib hl-bullet -lib heaps --main TestGravity --hl testgravity.hl
+hl testgravity.hl
 
 echo "Running TestGroundCollision"
-haxe -lib bullet -lib heaps --main TestGroundCollision --hl compiled.hl
-hl compiled.hl
+haxe -lib hl-bullet -lib heaps --main TestGroundCollision --hl testgroundcollision.hl
+hl testgroundcollision.hl
 
 echo "Running TestLinearVelocity"
-haxe -lib bullet -lib heaps --main TestLinearVelocity --hl compiled.hl
+haxe -lib hl-bullet -lib heaps --main TestLinearVelocity --hl compiled.hl
 hl compiled.hl
 
 echo "Running TestAngularVelocity"
-haxe -lib bullet -lib heaps --main TestAngularVelocity --hl compiled.hl
+haxe -lib hl-bullet -lib heaps --main TestAngularVelocity --hl compiled.hl
 hl compiled.hl
